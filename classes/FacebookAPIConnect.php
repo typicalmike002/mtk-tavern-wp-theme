7b8fa3dc106727de504c15bb5eaac298df4f42bc
Michael Townsend <townsendm105@gmail.com>
2016-03-24 20:08:15 -0400

    Update FacebookAPIConnect.php

--- classes/FacebookAPIConnect.php
+++ classes/FacebookAPIConnect.php
@@ -28,8 +28,8 @@ class FacebookAPIConnect {
 
 	public function __construct () {
 
-		$this->fb_page_id = "364219330272299";
-		$this->fb_app_id = "1705291236407017";
+		$this->fb_page_id = "Secret FB Page ID Number Goes Here";
+		$this->fb_app_id = "Secret FB App ID Number Goes Here";
 
 		$this->year_range = 2;
 		$this->since_date = date('Y-m-d');
@@ -38,7 +38,7 @@ class FacebookAPIConnect {
 		$this->since_unix_timestamp = strtotime( $this->since_date );
 		$this->until_unix_timestamp = strtotime( $this->until_date );
 
-		$this->access_token = "CAACEdEose0cBAAyKWhJpyyMJIZCif7C5xpClzTyfzgFrSmDraZAd2rQZAZBVxMzDxtZBhg5Rbnp3CqgvgUC1bukLtWFrDUibItAyNck6QjGjdPDEZBZAtUSkpDkuoepAOjv0U2ctkXc45GQXymacw1Go7DzEl6ZC1NxeSUHgMmNwqwxRE2859npggd8RTuAXjMSddV2cSMN3s55TCYZARB0KFQ0RFz9mUeS4ZD";
+		$this->access_token = "Secret Access Token Goes Here";
 
 		$this->fields = "id,name,description,timezone,start_time,cover";
 		$this->json_link = "https://graph.facebook.com/{$this->fb_page_id}/events/attending/?fields={$this->fields}&access_token={$this->access_token}&since={$this->since_unix_timestamp}&until={$this->until_unix_timestamp}";
