6185ecf0e1d4e9c0535096aadef2f4eb7c63f4b7
Michael Townsend <townsendm105@gmail.com>
2016-03-24 22:22:26 -0400

    Update contact-form.php

--- admin_templates/templates/contact-form.php
+++ admin_templates/templates/contact-form.php
@@ -1,11 +1,8 @@
 <?php
 /**
-<<<<<<< HEAD
  * Adds an input form for the contact us page on WordPress backend.  This email is used by the contact us form and will send all 
  * messages from that form to this email.
-=======
  * Adds the social media input form to WordPress's admin section
->>>>>>> 7b8fa3dc106727de504c15bb5eaac298df4f42bc
  *
  * @uses add_meta_box() For adding WordPress's built in form constructor.
  * @since MTK Tavern 1.0
@@ -14,25 +11,21 @@
 
 function meta_boxes() {
 
-<<<<<<< HEAD
 	add_meta_box( 'social_media_id', 'Social Media', 'load_contact_form_email', 'page', 'normal', 'high' );
 }
 
 function load_contact_form_email() {
-=======
+	
 	add_meta_box( 'social_media_id', 'Social Media', 'load_social_media_form', 'page', 'normal', 'high' );
-
 }
 
 function load_social_media_form() {
->>>>>>> 7b8fa3dc106727de504c15bb5eaac298df4f42bc
 
 	global $post;
 
 	//Validate form comes from this function.
 	wp_nonce_field( basename( __FILE__ ), 'nonce' );
 
-<<<<<<< HEAD
 	//Current Email Contact:
 	$values = get_post_meta( $post->ID, 'email_address', true );
 
@@ -44,8 +37,6 @@ function load_social_media_form() {
 		<label name="email_name"><p>Emails will be sent to the following email.</p></label>
 		<input type="email" required name="email_name" class="email_name" value="<?php echo esc_attr( $values['email'] ); ?>">
 	</form>
-
-=======
 	//Grabs the data saved in the post_meta.
 	$values = get_post_meta( $post->ID, 'social_media_urls', true );
 	?>
@@ -71,7 +62,6 @@ function load_social_media_form() {
 				title="URL Format: http://example.com">
 		</div>
 	</div>
->>>>>>> 7b8fa3dc106727de504c15bb5eaac298df4f42bc
 <?php }
 
 function save_forms ( $post_id ) {
@@ -84,18 +74,15 @@ function save_forms ( $post_id ) {
 	
 	if ( $is_autosave || $is_revision || !$is_valid_nonce || !$is_valid_user ) { return;/*Exits the function if the data is not safe: */}
 
-<<<<<<< HEAD
 	update_post_meta( $post_id, 'email_address', array( 
 		'email' =>  sanitize_text_field( $_POST['email_name'] )
 	) );
 
-=======
 	//Saves the data to the post_meta 
 	update_post_meta( $post_id, 'social_media_urls', array(
 		'facebook'	=>	sanitize_text_field ( $_POST['facebook'] ),
 		'instagram' =>	sanitize_text_field ( $_POST['instagram'] )
 	) );
->>>>>>> 7b8fa3dc106727de504c15bb5eaac298df4f42bc
 }
 
 remove_post_type_support( 'page', 'editor' );
